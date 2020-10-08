c032a20116afeae88555c44f81e8b2417eec4cde
Eliel Haouzi <eliel.haouzi@gmail.com>
2020-10-08 13:26:40 +0300

    map_from_struct/1

---
+++ test/ant_utils_ecto_test.exs
@@ -0,0 +1,49 @@
+defmodule AntlUtilsEctoTest do
+  use ExUnit.Case, async: false
+  use AntlUtilsEcto.DataCase
+  doctest AntlUtilsEcto
+
+  defmodule Schema do
+    use Ecto.Schema
+
+    schema "" do
+      field(:name, :string)
+      has_many(:children, Schema)
+    end
+
+    def changeset(%__MODULE__{} = schema, attrs) do
+      schema
+      |> Ecto.Changeset.cast(attrs, [:name])
+      |> Ecto.Changeset.cast_assoc(:children)
+    end
+  end
+
+  test "map_from_struct/1" do
+    schema =
+      %Schema{}
+      |> Schema.changeset(%{
+        name: "abraham",
+        children: [
+          %{
+            name: "isaac",
+            children: [%{name: "esau"}, %{name: "jacob"}]
+          }
+        ]
+      })
+      |> Ecto.Changeset.apply_changes()
+
+    terah = %{name: "terah", children: [schema]} |> AntlUtilsEcto.map_from_struct()
+
+    refute Map.has_key?(terah, :__struct__)
+
+    [abraham] = terah.children
+    refute Map.has_key?(abraham, :__struct__)
+
+    [isaac] = abraham.children
+    refute Map.has_key?(isaac, :__struct__)
+
+    [esau, jacob] = isaac.children
+    refute Map.has_key?(esau, :__struct__)
+    refute Map.has_key?(jacob, :__struct__)
+  end
+end
