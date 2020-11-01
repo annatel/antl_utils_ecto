1fe179ef49ace25ae165e9d66277f6f2aaa7f62b
Eliel Haouzi <eliel.haouzi@gmail.com>
2020-11-01 12:11:09 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AntlUtilsEcto.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/annatel/antl_utils_ecto"
-  @version "0.6.0"
+  @version "0.7.0"
 
   def project do
     [
