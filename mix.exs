07e50e5f3bb278acd73c132d5832912dee0a47b5
Laetitia <l.ohayon@bevaday.com>
2020-11-10 15:09:46 +0200

    revert version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AntlUtilsEcto.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/annatel/antl_utils_ecto"
-  @version "1.0.1"
+  @version "1.0"
 
   def project do
     [
