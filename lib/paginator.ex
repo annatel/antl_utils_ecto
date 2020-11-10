ea70c8c600d7967fab00adb9a8f2a59ff3fe17d8
Laetitia Ohayon <l.ohayon@bevaday.com>
2020-11-10 14:28:23 +0200

    improve pagination function signature (#2)
    * improve pagination function signature

--- lib/paginator.ex
+++ lib/paginator.ex
@@ -5,8 +5,6 @@ defmodule AntlUtilsEcto.Paginator do
 
   import Ecto.Query, only: [limit: 2, offset: 2]
 
-  @per_page 100
-
   @doc """
   Paginate.
 
@@ -14,24 +12,23 @@ defmodule AntlUtilsEcto.Paginator do
 
     iex> queryable = from u in "products"
     #Ecto.Query<from p0 in "products">
-    iex> Paginator.paginate(queryable, 1)
+    iex> Paginator.paginate(queryable, 1, 100)
     #Ecto.Query<from p0 in "products", limit: ^100, offset: ^0>
-    iex> Paginator.paginate(queryable, 2)
+    iex> Paginator.paginate(queryable, 2, 100)
     #Ecto.Query<from p0 in "products", limit: ^100, offset: ^100>
-    iex> Paginator.paginate(queryable, 3)
+    iex> Paginator.paginate(queryable, 3, 100)
     #Ecto.Query<from p0 in "products", limit: ^100, offset: ^200>
-    iex> Paginator.paginate(queryable, 3, [per_page: 10])
+    iex> Paginator.paginate(queryable, 3, 10)
     #Ecto.Query<from p0 in "products", limit: ^10, offset: ^20>
 
   """
-  @spec paginate(any, number, keyword) :: Ecto.Query.t()
-  def paginate(queryable, page, opts \\ []) when page > 0 do
-    per_page = Keyword.get(opts, :per_page, @per_page)
 
-    offset = per_page * (page - 1)
+  @spec paginate(any, pos_integer(), pos_integer()) :: Ecto.Query.t()
+  def paginate(queryable, page_number, page_size) when page_number > 0 and page_size > 0 do
+    offset = page_size * (page_number - 1)
 
     queryable
-    |> limit(^per_page)
+    |> limit(^page_size)
     |> offset(^offset)
   end
 end
