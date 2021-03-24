086b6f1f7130925f5a961adbf3b4719d6d6a0c5d
Laetitia <l.ohayon@bevaday.com>
2021-03-24 14:09:56 +0200

    upgrade antl_utils_elixir

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AntlUtilsEcto.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/annatel/antl_utils_ecto"
-  @version "1.2.1"
+  @version "1.3.0"
 
   def project do
     [
@@ -32,7 +32,7 @@ defmodule AntlUtilsEcto.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:ecto, "~> 3.0"},
-      {:antl_utils_elixir, "~> 0.2.0"}
+      {:antl_utils_elixir, "~> 0.3.0"}
     ]
   end
 
