f860043b4666c717a4b6dff9933d3e4ade881e31
Laetitia <l.ohayon@bevaday.com>
2021-06-17 10:15:57 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AntlUtilsEcto.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/annatel/antl_utils_ecto"
-  @version "2.2.0"
+  @version "2.2.1"
 
   def project do
     [
