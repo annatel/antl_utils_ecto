de1d889f438e9c03c57af9501372917835363171
Laetitia <l.ohayon@bevaday.com>
2021-07-14 09:40:21 +0300

    refacto map_from_struct

--- lib/antl_utils_ecto.ex
+++ lib/antl_utils_ecto.ex
@@ -2,31 +2,45 @@ defmodule AntlUtilsEcto do
   @doc """
   Recursive Schema to map function.
   """
+
   @spec map_from_struct(map) :: map
   def map_from_struct(schema) when is_struct(schema) do
     schema
+    |> to_map()
+  end
+
+  defp to_map(schema) when is_struct(schema) do
+    associations =
+      try do
+        schema.__struct__.__schema__(:associations)
+      rescue
+        UndefinedFunctionError -> []
+      end
+
+    embeds =
+      try do
+        schema.__struct__.__schema__(:embeds)
+      rescue
+        UndefinedFunctionError -> []
+      end
+
+    schema
     |> Map.from_struct()
-    |> map_from_struct()
-  end
-
-  def map_from_struct(map) when is_map(map) do
-    map
-    |> Enum.reduce(%{}, fn {key, value}, acc ->
-      value =
-        case value do
-          value when is_struct(value) ->
-            map_from_struct(value)
-
-          value when is_list(value) ->
-            value |> Enum.map(&map_from_struct/1)
-
-          _ ->
-            value
-        end
-
-      Map.put_new(acc, key, value)
+    |> Map.delete(:__meta__)
+    |> Enum.map(fn {key, value} ->
+      if key in (associations ++ embeds), do: {key, to_map(value)}, else: {key, value}
     end)
+    |> Enum.into(%{})
   end
 
-  def map_from_struct(value), do: value
+  defp to_map(map) when is_map(map) do
+    Enum.map(map, fn {key, value} -> {key, to_map(value)} end)
+    |> Enum.into(%{})
+  end
+
+  defp to_map(list) when is_list(list) do
+    Enum.map(list, &to_map/1)
+  end
+
+  defp to_map(value), do: value
 end
---
+++ test/antl_utils_ecto_test.exs
@@ -0,0 +1,92 @@
+defmodule AntlUtilsEctoTest do
+  use AntlUtilsEcto.DataCase
+  doctest AntlUtilsEcto
+
+  defmodule Schema do
+    use Ecto.Schema
+
+    schema "" do
+      field(:name, :string)
+      field(:wives, {:array, :string}, default: [])
+      belongs_to(:parent, Schema)
+      has_many(:children, Schema)
+      field(:birthday, :utc_datetime, default: DateTime.utc_now())
+      field(:rdvs, {:array, :utc_datetime}, default: [])
+    end
+  end
+
+  defmodule EmbeddedSchema do
+    use Ecto.Schema
+
+    embedded_schema do
+      field(:name, :string)
+      field(:wives, {:array, :string}, default: [])
+      embeds_one(:parent, EmbeddedSchema)
+      embeds_many(:children, EmbeddedSchema)
+      field(:birthday, :utc_datetime, default: DateTime.utc_now())
+      field(:rdvs, {:array, :utc_datetime}, default: [])
+    end
+  end
+
+  describe "map_from_struct/1" do
+    test "for ecto schema" do
+      abraham =
+        %Schema{
+          name: "abraham",
+          wives: ["sarah", "hagar"],
+          parent: %Schema{name: "terah"},
+          birthday: DateTime.utc_now(),
+          children: [
+            %Schema{
+              name: "isaac",
+              children: [%Schema{name: "esau"}, %Schema{name: "jacob"}]
+            }
+          ],
+          rdvs: [DateTime.utc_now(), DateTime.utc_now()]
+        }
+        |> AntlUtilsEcto.map_from_struct()
+
+      refute Map.has_key?(abraham, :__struct__)
+      assert %DateTime{} = abraham.birthday
+
+      [isaac] = abraham.children
+      refute Map.has_key?(isaac, :__struct__)
+
+      [esau, jacob] = isaac.children
+      refute Map.has_key?(esau, :__struct__)
+      refute Map.has_key?(jacob, :__struct__)
+
+      Enum.each(abraham.rdvs, &assert(%DateTime{} = &1))
+    end
+
+    test "for ecto embedded_schema" do
+      abraham =
+        %EmbeddedSchema{
+          name: "abraham",
+          wives: ["sarah", "hagar"],
+          parent: %EmbeddedSchema{name: "terah"},
+          birthday: DateTime.utc_now(),
+          children: [
+            %EmbeddedSchema{
+              name: "isaac",
+              children: [%EmbeddedSchema{name: "esau"}, %EmbeddedSchema{name: "jacob"}]
+            }
+          ],
+          rdvs: [DateTime.utc_now(), DateTime.utc_now()]
+        }
+        |> AntlUtilsEcto.map_from_struct()
+
+      refute Map.has_key?(abraham, :__struct__)
+      assert %DateTime{} = abraham.birthday
+
+      [isaac] = abraham.children
+      refute Map.has_key?(isaac, :__struct__)
+
+      [esau, jacob] = isaac.children
+      refute Map.has_key?(esau, :__struct__)
+      refute Map.has_key?(jacob, :__struct__)
+
+      Enum.each(abraham.rdvs, &assert(%DateTime{} = &1))
+    end
+  end
+end
