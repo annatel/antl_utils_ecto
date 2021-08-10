3ee28f2160eb4c7f70d43c9f4d307fe5ab4b6506
Laetitia <l.ohayon@bevaday.com>
2021-08-10 09:47:35 +0300

    add on_valid_changeset/2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule AntlUtilsEcto.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/annatel/antl_utils_ecto"
-  @version "2.4.3"
+  @version "2.4.4"
 
   def project do
     [
