36b0a05bcfe9112f7bf929dae4c6c0b9c6e180bd
Eliel Haouzi <eliel.haouzi@gmail.com>
2021-12-11 17:56:15 +0200

    add support to ecto schema through rpc calls

--- lib/antl_utils_ecto.ex
+++ lib/antl_utils_ecto.ex
@@ -10,37 +10,35 @@ defmodule AntlUtilsEcto do
   end
 
   defp to_map(schema) when is_struct(schema) do
-    associations =
-      try do
-        schema.__struct__.__schema__(:associations)
-      rescue
-        UndefinedFunctionError -> []
-      end
-
-    embeds =
-      try do
-        schema.__struct__.__schema__(:embeds)
-      rescue
-        UndefinedFunctionError -> []
-      end
-
-    schema
-    |> Map.from_struct()
-    |> Map.delete(:__meta__)
-    |> Enum.map(fn {key, value} ->
-      if key in (associations ++ embeds), do: {key, to_map(value)}, else: {key, value}
-    end)
-    |> Enum.into(%{})
+    if not ecto_schema?(schema) do
+      schema
+    else
+      schema
+      |> Map.from_struct()
+      |> Enum.map(fn {key, value} -> {key, to_map(value)} end)
+      |> Enum.into(%{})
+    end
   end
 
-  defp to_map(map) when is_map(map) do
-    Enum.map(map, fn {key, value} -> {key, to_map(value)} end)
-    |> Enum.into(%{})
+  defp to_map(value) do
+    cond do
+      is_map(value) -> to_map(value)
+      is_list(value) -> Enum.map(value, &to_map/1)
+      true -> value
+    end
   end
 
-  defp to_map(list) when is_list(list) do
-    Enum.map(list, &to_map/1)
+  defp ecto_schema?(%{__meta__: %{__struct__: Ecto.Schema.Metadata}}), do: true
+
+  defp ecto_schema?(schema) when is_struct(schema) do
+    try do
+      schema.__struct__.__schema__(:source)
+    rescue
+      UndefinedFunctionError -> false
+    else
+      _ -> true
+    end
   end
 
-  defp to_map(value), do: value
+  defp ecto_schema?(_), do: false
 end
--- test/antl_utils_ecto_test.exs
+++ test/antl_utils_ecto_test.exs
@@ -59,7 +59,7 @@ defmodule AntlUtilsEctoTest do
       Enum.each(abraham.rdvs, &assert(%DateTime{} = &1))
     end
 
-    test "for ecto embedded_schema" do
+    test "for local ecto embedded_schema" do
       abraham =
         %EmbeddedSchema{
           name: "abraham",
@@ -70,6 +70,55 @@ defmodule AntlUtilsEctoTest do
             %EmbeddedSchema{
               name: "isaac",
               children: [%EmbeddedSchema{name: "esau"}, %EmbeddedSchema{name: "jacob"}]
+            }
+          ],
+          rdvs: [DateTime.utc_now(), DateTime.utc_now()]
+        }
+        |> AntlUtilsEcto.map_from_struct()
+
+      refute Map.has_key?(abraham, :__struct__)
+      assert %DateTime{} = abraham.birthday
+
+      [isaac] = abraham.children
+      refute Map.has_key?(isaac, :__struct__)
+
+      [esau, jacob] = isaac.children
+      refute Map.has_key?(esau, :__struct__)
+      refute Map.has_key?(jacob, :__struct__)
+
+      Enum.each(abraham.rdvs, &assert(%DateTime{} = &1))
+    end
+
+    test "for remote ecto embedded_schema " do
+      abraham =
+        %{
+          __meta__: %Ecto.Schema.Metadata{},
+          __struct__: RemoteApp.Module.EmbeddedSchema,
+          name: "abraham",
+          wives: ["sarah", "hagar"],
+          parent: %{
+            __meta__: %Ecto.Schema.Metadata{},
+            __struct__: RemoteApp.Module.EmbeddedSchema,
+            name: "terah"
+          },
+          birthday: DateTime.utc_now(),
+          children: [
+            %{
+              __meta__: %Ecto.Schema.Metadata{},
+              __struct__: RemoteApp.Module.EmbeddedSchema,
+              name: "isaac",
+              children: [
+                %{
+                  __meta__: %Ecto.Schema.Metadata{},
+                  __struct__: RemoteApp.Module.EmbeddedSchema,
+                  name: "esau"
+                },
+                %{
+                  __meta__: %Ecto.Schema.Metadata{},
+                  __struct__: RemoteApp.Module.EmbeddedSchema,
+                  name: "jacob"
+                }
+              ]
             }
           ],
           rdvs: [DateTime.utc_now(), DateTime.utc_now()]
